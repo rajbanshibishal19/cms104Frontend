77fec3987aa6800cb1bd504f37e5a316b4251a0d
Bishal Rajbanshi <rajbanshibishal2019@gmail.com>
2023-10-16 15:48:06 +0545

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <nav>
+    <div className="navbar">
+        <a href="#" className="logo">Your Logo</a>
+        <ul className="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </div>
+</nav>
+  )
+}
+
+export default Home
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Home
